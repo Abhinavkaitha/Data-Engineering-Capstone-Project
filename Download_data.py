0b95bd2d8a5c6dda2d9d2141cb11511b81b225ac
Abhinav Reddy Kaitha <ark@Abhinavs-MacBook-Pro.local>
2020-01-25 05:10:38 -0800

    space_error

--- Download_data.py
+++ Download_data.py
@@ -1,5 +1,6 @@
 import os
 with open("list_files.txt") as f:
     for file in f:
+    	file = file.rstrip("\n")
         os.system("wget -O- 'https://archive.org/download/archiveteam-twitter-stream-2018-{}".format(file[13:15])+"/{}'".format(file)+"\
         | aws s3 cp - s3://tone-of-the-nation/twitter_dump/{}".format(file))
