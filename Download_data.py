493fd91a672c88d27cf78689fd841ad99a505503
Abhinav Reddy Kaitha <ark@Abhinavs-MacBook-Pro.local>
2020-01-25 03:29:55 -0800

    Download-Data

---
+++ Download_data.py
@@ -0,0 +1,5 @@
+import os
+with open("list_files.txt") as f:
+    for file in f:
+        os.system("wget -O- 'https://archive.org/download/archiveteam-twitter-stream-2018-{}".format(file[13:15])+"/{}'".format(file)+"\
+        | aws s3 cp - s3://tone-of-the-nation/twitter_dump/{}".format(file))
