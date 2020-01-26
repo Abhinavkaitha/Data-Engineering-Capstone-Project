8e476b8a2c3cadf6182ebe678dac94c92fae3089
Abhinav Reddy Kaitha <ark@Abhinavs-MacBook-Pro.local>
2020-01-26 03:58:04 -0800

    decompress

--- Decompress.py
+++ Decompress.py
@@ -2,5 +2,7 @@ import os
 with open("list_files.txt") as f:
     for file in f:
         file = file.rstrip("\n")
-        #print("wget -O- 'https://archive.org/download/archiveteam-twitter-stream-2018-{}/{}'| aws s3 cp - s3://tone-of-the-nation/twitter_dump/{}".format(file[13:15],file,file))
+        #print("mkdir /mys3bucket/twitter_bz2/{}".format(file[:-4]))
+        #print("tar -xvf /mys3bucket/twitter_dump/{} -C /mys3bucket/twitter_bz2/{}/".format(file,file[:-4]))
+        os.system("mkdir /mys3bucket/twitter_bz2/{}".format(file[:-4]))
         os.system("wget -O- 'https://archive.org/download/archiveteam-twitter-stream-2018-{}".format(file[13:15])+"/{}'".format(file)+"| aws s3 cp - s3://tone-of-the-nation/twitter_dump/{}".format(file))
