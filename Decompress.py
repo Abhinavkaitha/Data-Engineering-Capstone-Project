412705b94d89ee643417c6913ea8edd90b9ab95a
Abhinav Reddy Kaitha <kaitha.abhi@gmail.com>
2020-01-26 13:45:56 -0800

    Download-Decompress

--- Decompress.py
+++ Decompress.py
@@ -2,7 +2,5 @@ import os
 with open("list_files.txt") as f:
     for file in f:
         file = file.rstrip("\n")
-        #print("mkdir /mys3bucket/twitter_bz2/{}".format(file[:-4]))
-        #print("tar -xvf /mys3bucket/twitter_dump/{} -C /mys3bucket/twitter_bz2/{}/".format(file,file[:-4]))
         os.system("mkdir /mys3bucket/twitter_bz2/{}".format(file[:-4]))
         os.system("tar -xvf /mys3bucket/twitter_dump/{} -C /mys3bucket/twitter_bz2/{}/".format(file,file[:-4]))
--- Download_data.py
+++ Download_data.py
@@ -2,5 +2,4 @@ import os
 with open("list_files.txt") as f:
     for file in f:
         file = file.rstrip("\n")
-        #print("wget -O- 'https://archive.org/download/archiveteam-twitter-stream-2018-{}/{}'| aws s3 cp - s3://tone-of-the-nation/twitter_dump/{}".format(file[13:15],file,file))
         os.system("wget -O- 'https://archive.org/download/archiveteam-twitter-stream-2018-{}".format(file[13:15])+"/{}'".format(file)+"| aws s3 cp - s3://tone-of-the-nation/twitter_dump/{}".format(file))
