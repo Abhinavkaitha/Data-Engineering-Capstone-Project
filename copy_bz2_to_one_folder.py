14c963505525452f2523d5738586b5095ffe6c10
Abhinav Reddy Kaitha <kaitha.abhi@gmail.com>
2020-01-27 17:06:38 -0800

    copy_all_bz2_to_one_folder

--- copy_bz2_to_one_folder.py
+++ copy_bz2_to_one_folder.py
@@ -1,13 +1,9 @@
 import os
-import glob
 
-# checking your current working directory
-#print(os.getcwd())
 
-# Get your current folder and subfolder event data
-my_path = os.getcwd() + '/Downloads/twitter_bz2'
+my_path = '/mys3bucket/twitter_bz2'
 
-# Create a for loop to create a list of files and collect each filepath
+# Create a for loop to create a list of all bz2 files and collect each filepath
 
 all_files = []
 for dirpath, dirs, files in os.walk(my_path):
@@ -19,5 +15,5 @@ for dirpath, dirs, files in os.walk(my_path):
 print("Total Number of bz2 files: ", len(all_files))
 
 for i in all_files:
-    new_name = i[33:].replace("/", "_")
-    os.system("mv "+ i + " "+"/Users/ark/Downloads/twitter_parquet/"+new_name)
+    new_name = i[25:].replace("/", "_")
+    os.system("mv "+ i + " "+"/mys3bucket/twitter_bz2_dump/"+new_name)
