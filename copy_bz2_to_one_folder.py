d44f4cf77e24164440ac3100fb7c366058b7f526
Abhinav Reddy Kaitha <kaitha.abhi@gmail.com>
2020-01-28 11:15:18 -0800

    variable_changed

--- copy_bz2_to_one_folder.py
+++ copy_bz2_to_one_folder.py
@@ -3,7 +3,6 @@ import os
 
 my_path = '/mys3bucket/twitter_bz2'
 
-# Create a for loop to create a list of all bz2 files and collect each filepath
 count = 0
 all_files = []
 for dirpath, dirs, files in os.walk(my_path):
@@ -12,13 +11,6 @@ for dirpath, dirs, files in os.walk(my_path):
     if fname.endswith('.bz2'):
         new_name = fname[25:].replace("/", "_")
         count += 1
-        os.system("mv " + i + " " + "/mys3bucket/twitter_bz2_dump/" + new_name)
+        os.system("mv " + fname + " " + "/mys3bucket/twitter_bz2_dump/" + new_name)
         if count%1000 == 0:
             print("Number of files moved:", count)
-#         all_files.append(fname)
-#
-# print("Total Number of bz2 files: ", len(all_files))
-#
-# for i in all_files:
-#     new_name = i[25:].replace("/", "_")
-#     os.system("mv "+ i + " "+"/mys3bucket/twitter_bz2_dump/"+new_name)
