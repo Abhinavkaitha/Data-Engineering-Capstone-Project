21ffb006315389caa14b5ecfa6db64ec4798acad
Abhinav Reddy Kaitha <kaitha.abhi@gmail.com>
2020-02-03 20:16:42 -0800

    moveto_mulltiple_folders

--- src/visualization/app.py
+++ src/visualization/app.py
@@ -10,18 +10,18 @@ from datetime import datetime
 
 app = dash.Dash()
 
-db='abkaitha'
-user='abkaitha'
-host='tone-of-the-nation.c8ubsnm2laco.us-west-2.rds.amazonaws.com'
-password='abkaitha'
-port='5432'
+db = 'abkaitha'
+user = 'abkaitha'
+host = 'tone-of-the-nation.c8ubsnm2laco.us-west-2.rds.amazonaws.com'
+password = 'abkaitha'
+port = '5432'
 
-url = 'postgresql://{}:{}@{}:{}/{}'.format(user,password,host,port,db)
+url = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db)
 
 con = create_engine(url)
 
-features = ['Coca-Cola','Pepsi']
-hours = list(range(1,25))
+features = ['Coca-Cola', 'Pepsi']
+hours = list(range(1, 25))
 days = ['Mon',
         'Tue',
         'Wed',
@@ -31,45 +31,41 @@ days = ['Mon',
         'Sun']
 
 app.layout = html.Div([
-
-        html.Div([
-            dcc.Dropdown(
-                id='company',
-                options=[{'label': i, 'value': i} for i in features],
-                value='Coca-Cola'
-            )
-        ],
-        style={'width': '28%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='company',
+            options=[{'label': i, 'value': i} for i in features],
+            value='Coca-Cola'
+        )
+    ], style={'width': '28%', 'display': 'inline-block'}),
 
     html.Div([
-        
         dcc.DatePickerRange(
             id='my_date_picker',
             min_date_allowed=datetime(2018, 5, 2),
             max_date_allowed=datetime(2018, 10, 31),
             start_date=datetime(2018, 5, 2),
-            end_date=datetime(2018, 5, 3)
+            end_date=datetime(2018, 10, 3)
         )
     ], style={'display': 'inline-block'}),
 
-        html.Div([
-            dcc.Dropdown(
-                id='hour',
-                options=[{'label': i, 'value': i} for i in hours],
-                value=14
-            )
-        ],style={'width': '18%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='hour',
+            options=[{'label': i, 'value': i} for i in hours],
+            value=14
+        )
+    ], style={'width': '18%', 'display': 'inline-block'}),
 
-        html.Div([
-            dcc.Dropdown(
-                id='day',
-                options=[{'label': i, 'value': i} for i in days],
-                value='Mon'
-            )
-        ],style={'width': '18%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='day',
+            options=[{'label': i, 'value': i} for i in days],
+            value='Mon'
+        )
+    ], style={'width': '18%', 'display': 'inline-block'}),
 
-    dcc.Graph(id='feature-graphic')
-], style={'padding':10})
+        dcc.Graph(id='feature-graphic')], style={'padding': 10})
 
 @app.callback(
     Output('feature-graphic', 'figure'),
@@ -78,9 +74,9 @@ app.layout = html.Div([
     Input('my_date_picker','end_date'),
      Input('my_date_picker','end_date'),
      Input('day','value')])
-def update_graph(company_name, hour_of_the_day,start_date,end_date,day):
+def update_graph(company_name, hour_of_the_day, start_date, end_date, day):
 
-    states = pd.read_sql("SELECT * FROM tone WHERE tone.date BETWEEN '{}' AND '{}' AND tone.company = '{}' AND tone.hour={} AND tone.day='{}'".format(start_date,end_date,company_name,hour_of_the_day,day),con)
+    states = pd.read_sql("SELECT * FROM tone WHERE tone.date BETWEEN '{}' AND '{}' AND tone.company = '{}' AND tone.hour={} AND tone.day='{}'".format(start_date, end_date, company_name, hour_of_the_day, day), con)
     mean_df = states.groupby('location').mean()
     mean_tone = mean_df['tone']
     valid_states = list(mean_df.index)
@@ -92,9 +88,9 @@ def update_graph(company_name, hour_of_the_day,start_date,end_date,day):
     colorscale = 'Reds',
     colorbar_title = 'Sentiment')
                                  ],
-    'layout':go.Layout(
+    'layout': go.Layout(
     title_text = 'Tone of the Nation',
-    geo_scope='usa')
+    geo_scope = 'usa')
     }
 
 
