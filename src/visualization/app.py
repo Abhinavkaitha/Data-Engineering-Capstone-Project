bbdc2f72028982a6542a6a895fe7b2641eaeeb19
Abhinav Reddy Kaitha <kaitha.abhi@gmail.com>
2020-02-17 22:45:40 -0800

    app

--- src/visualization/app.py
+++ src/visualization/app.py
@@ -49,13 +49,13 @@ app.layout = html.Div([
         )
     ], style={'display': 'inline-block'}),
 
-    # html.Div([
-    #     dcc.Dropdown(
-    #         id='hour',
-    #         options=[{'label': i, 'value': i} for i in hours],
-    #         value=14
-    #     )
-    # ], style={'width': '18%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='hour',
+            options=[{'label': i, 'value': i} for i in hours],
+            value=14
+        )
+    ], style={'width': '18%', 'display': 'inline-block'}),
 
     html.Div([
         dcc.Dropdown(
@@ -72,13 +72,13 @@ app.layout = html.Div([
 @app.callback(
     Output('feature-graphic', 'figure'),
     [Input('company', 'value'),
-     # Input('hour', 'value'),
+     Input('hour', 'value'),
     Input('my_date_picker','end_date'),
      Input('my_date_picker','end_date'),
      Input('day','value')])
-def update_graph(company_name,  start_date, end_date, day):
+def update_graph(company_name, hour_of_the_day, start_date, end_date, day):
 
-    states = pd.read_sql("SELECT * FROM tone1 WHERE tone1.date BETWEEN '{}' AND '{}' AND tone1.company = '{}'  AND tone1.day='{}'".format(start_date, end_date, company_name, day), con)
+    states = pd.read_sql("SELECT * FROM tone1 WHERE tone1.date BETWEEN '{}' AND '{}' AND tone1.company = '{}' AND tone1.hour={} AND tone1.day='{}'".format(start_date, end_date, company_name, hour_of_the_day, day), con)
     mean_df = states.groupby('location').mean()
     mean_tone = mean_df['tone']
     valid_states = list(mean_df.index)
